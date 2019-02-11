db95751b2644d1faad92748aded623a9c17d3084
Daniel Palmer <daniel@0x0f.com>
2019-02-11 19:54:57 +0900

    Split mqttbase out of tlwpy

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mqttbase',
+      version='0.1',
+      author='Daniel Palmer',
+      author_email='daniel@0x0f.com',
+      license='GPLv3',
+      packages=['mqttbase'],
+      zip_safe=False)
